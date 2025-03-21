3a491b94d4f5e675ff4e4ed2810ec633f816c5df
happex <110610727+okayhappex@users.noreply.github.com>
2025-03-21 17:10:59 +0100

    Espaces => Tabs

--- utils/common/drive.py
+++ utils/common/drive.py
@@ -5,22 +5,22 @@ from utils.db import drivepath
 from .commons import *
 
 def register_file(_dir: str, name: str, data: bytes, overwrite: bool = False):
-    _dir = os.path.join(drivepath, name)
-    path = os.path.join(_dir, name)
+	_dir = os.path.join(drivepath, name)
+	path = os.path.join(_dir, name)
 
-    if not os.path.exists(_dir): # La database est bien structurée, aucune création de dossier permise
-        raise PermissionError(f"Attempt to create a directory: {path}")
+	if not os.path.exists(_dir): # La database est bien structurée, aucune création de dossier permise
+		raise PermissionError(f"Attempt to create a directory: {path}")
 
-    if os.path.exists(path) and not overwrite:
-        raise FileExistsError(f"File {path} already exists.")
+	if os.path.exists(path) and not overwrite:
+		raise FileExistsError(f"File {path} already exists.")
 
-    with open(path, "wb") as file:
-        file.write(data)
+	with open(path, "wb") as file:
+		file.write(data)
 
 def open_file(path: str) -> bytes:
-    path = adjust_path(path)
-    if path.startswith('/'): path = path[1:]
-    path = os.path.join(drivepath, path)
+	path = adjust_path(path)
+	if path.startswith('/'): path = path[1:]
+	path = os.path.join(drivepath, path)
 
-    with open(path, "rb") as file:
-        return file.read()
+	with open(path, "rb") as file:
+		return file.read()
