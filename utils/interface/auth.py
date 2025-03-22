65c200e8d3437f07843b89fa9b6386ee02409139
happex <110610727+okayhappex@users.noreply.github.com>
2025-03-22 14:44:44 +0100

    + interface auth

---
+++ utils/interface/auth.py
@@ -0,0 +1,40 @@
+import hashlib
+import random
+import time
+
+from flask import Request
+
+from utils.common.commons import *
+from utils.functions import auth
+from utils.functions import server
+
+def ask_token(req: Request):
+	payload = req.json
+
+	if 'username' not in payload.keys() or 'password' not in payload.keys():
+		server.error(req.remote_addr, 'POST', '/auth/login', 401, "Missing Credentials")
+		return {"message": "Missing Credentials"}, 401
+
+	if not(sql_safe(payload["username"]) and sql_safe(payload["password"])): # On détecte les menaces dans l'entrée utilisateur
+		return {"message": "Incorrect Credentials"}, 401
+
+	acc = auth.get_account(payload['username'])
+
+	if not acc:
+		server.error(req.remote_addr, 'POST', '/auth/login', 401, "Account Does Not Exist")
+		return {"message": "Incorrect Credentials"}, 401
+
+	if acc['pwd'] == hashlib.sha256(payload['password'].encode()).hexdigest():
+		token = ''.join([ chr(random.randint(65, 90)) for _ in range(256) ])
+		auth.save_session({
+			"id": round(time.time()) * random.randint(0, 256),
+			"token": token,
+			"author": acc["author_id"]
+		})
+
+		server.log(req.remote_addr,  'POST','/auth/login')
+		server.create_archive("auth", {"action": "GENERATE_TOKEN", "ip_addr": req.remote_addr, "author": acc["author_id"]})
+		return {"token": token}, 200
+	else:
+		server.error(req.remote_addr, 'POST', '/auth/login', 401, "Incorrect Password")
+		return {"message": "Incorrect Credentials"}, 401
