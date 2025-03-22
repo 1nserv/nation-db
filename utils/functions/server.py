a29ffa2c04e72ad75592e7cc5e904e2e5d3f760f
happex <110610727+okayhappex@users.noreply.github.com>
2025-03-22 16:50:19 +0100

    Affichage plus lisible

--- utils/functions/server.py
+++ utils/functions/server.py
@@ -4,27 +4,27 @@ import os
 from utils.db import logpath
 
 def error(ip: str, method: str, path: str, code: int = 500, message: str = "Unknown Error"):
-	date = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+	date = datetime.now().strftime("%Y.%m.%d %H:%M:%S")
 
-	output = f"\033[1;34m{ip}\033[0m" + (16 - len(ip)) * " " + f"at {date} | \033[31;1m{code}\033[0m {method} {path}"
+	output = f"\033[1;34m{ip}" + (15 - len(ip)) * "-" + f" \033[0m{date} | \033[31;1m{code}\033[0m {method} {path}"
 	print(output)
 
 	with open(os.path.join(logpath, "requests.log"), "a") as file:
-		file.write(f"\n{ip}" + (16 - len(ip)) * " " + f"at {date} | {method} {path} - {code} {message}")
+		file.write(f"\n{ip}" + (15 - len(ip)) * "-" + f" {date} | {method} {path} - {code} {message}")
 
 def log(ip: str, method: str, path: str, code: int = 200, message: str = "Success"):
-	date = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+	date = datetime.now().strftime("%Y.%m.%d %H:%M:%S")
 
-	output = f"\033[1;34m{ip}\033[0m" + (16 - len(ip)) * " " + f"at {date} | \033[32;1m{code}\033[0m {method} {path}"
+	output = f"\033[1;34m{ip}" + (15 - len(ip)) * "-" + f"\033[0m {date} | \033[32;1m{code}\033[0m {method} {path}"
 	print(output)
 
 	with open(os.path.join(logpath, "requests.log"), "a") as file:
-		file.write(f"\n{ip}" + (16 - len(ip)) * " " + f"at {date} | {method} {path} - {code} {message}")
+		file.write(f"\n{ip}" + (15 - len(ip)) * "-" + f" {date} | {method} {path} - {code} {message}")
 
 
 def create_archive(domain: str, archive: dict):
 	name = datetime.now().strftime("%H_%M_%S.log")
-	date = datetime.now().strftime("%d/%m/%Y à %H/%M/%S")
+	date = datetime.now().strftime("%Y/%m/%d %H/%M/%S")
 	dir = os.path.join(logpath, domain, str(datetime.now().year), str(datetime.now().month), str(datetime.now().day))
 
 	if not os.path.exists(dir):
