40fb63b81502fd2d3c406440c2c1feef9329ed9d
happex <110610727+okayhappex@users.noreply.github.com>
2025-04-21 16:08:26 +0200

    Ajout d'une fonction qui explore le marketplace

--- utils/interface/economy/sales.py
+++ utils/interface/economy/sales.py
@@ -1,3 +1,5 @@
+import urllib
+
 from flask import Request
 
 from utils.common.commons import *
@@ -93,3 +95,49 @@ def cancel_sale(req: Request, id: str):
 	})
 
 	return {"message": "Sale Successfully Cancelled"}, 200
+
+
+def search_sales(req: Request):
+	params = req.args
+
+	for k, v in params.items():
+		if not (tn_safe(k) and sql_safe(v)):
+			server.error(req.remote_addr, "POST", f"/fetch/sales", 400, "Invalid Params")
+			return {"message": "Invalid Params"}, 400
+
+		v = urllib.parse.unquote(v)
+
+	if req.authorization:
+		token = req.authorization.token
+	else:
+		server.error(req.remote_addr, 'POST', f'/fetch/sales', 401, "Unauthorized")
+		return {"message": "Unauthorized"}, 401
+
+	if token:
+		if not sql_safe(token):
+			server.error(req.remote_addr, 'GET', f'/fetch/sales', 400, "Bad Request")
+			return {"message": "Bad Request"}, 400
+
+		if not auth.check_session(token, { "marketplace": "---r" }):
+			server.error(req.remote_addr, 'GET', f'/fetch/sales', 403, "Forbidden")
+			return {"message": "Forbidden"}, 403
+	else:
+		server.error(req.remote_addr, 'GET', f'/fetch/sales', 401, "Unauthorized")
+		return {"message": "Unauthorized"}, 401
+
+	# ---- FOUILLE DANS LA DB ----
+
+	data = economy.fetch_sales(**params)
+
+	res = []
+
+	for sale in data:
+		if not sale: continue
+
+		_sale = get_sale(req, sale['id'])
+
+		if _sale[1] == 200:
+			res.append(_sale[0])
+
+	server.log(req.remote_addr, 'GET', f'/fetch/sales')
+	return res, 200
