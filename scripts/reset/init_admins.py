67cb18c29b2c022891d0396bbec9442013ceb2cf
happex <110610727+okayhappex@users.noreply.github.com>
2025-04-27 19:38:06 +0200

    Suppression d'un admin
    * Cet admin a été destitué de ses fonctions il y a quelques mois suite à une purge.

--- scripts/reset/init_admins.py
+++ scripts/reset/init_admins.py
@@ -4,7 +4,6 @@ ef.delete_individual('BDF22789F400050')
 ef.delete_individual('F7DB60DD1C4300A')
 ef.delete_individual('A5FBDCE1D000078')
 ef.delete_individual('B40AC2137440001')
-ef.delete_individual('BC5905AE5D0001E')
 
 member = {
 	"id": "BDF22789F400050",
@@ -48,20 +47,6 @@ ef.save_individual(member)
 member = {
 	"id": "B40AC2137440001",
 	"name": "Akel",
-	"position": "admin",
-	"register_date": 0,
-	"xp": 10000,
-	"boosts": {},
-	"additional": {},
-	"votes": []
-}
-
-ef.save_individual(member)
-
-
-member = {
-	"id": "BC5905AE5D0001E",
-	"name": "Alexis",
 	"position": "admin",
 	"register_date": 0,
 	"xp": 10000,
